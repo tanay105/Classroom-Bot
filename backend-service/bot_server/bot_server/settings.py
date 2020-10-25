ec8c5cb03e3d7ac4adb39d320a390ac4c1bc62c0
Tanay Agrawal <tagrawa3@ncsu.edu>
2020-10-25 05:23:16 -0400

    ISSUE:19 adding changes to docker start command

--- backend-service/bot_server/bot_server/settings.py
+++ backend-service/bot_server/bot_server/settings.py
@@ -153,9 +153,3 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 # Use nose to run all tests
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-
-# Tell nose to measure coverage on the 'foo' and 'bar' apps
-#NOSE_ARGS = [
-#    '--with-coverage',
-#    '--cover-package=api',
-#]
