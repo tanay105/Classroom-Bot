e1897912d6bf1faace9c89fc08be6940c5a00870
Tanay Agrawal <tagrawa3@ncsu.edu>
2020-10-25 06:47:57 -0400

    ISSUE:19 adding container start script

--- backend-service/test_script.sh
+++ backend-service/test_script.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 python3 manage.py test api --with-coverage --cover-package=api --verbosity=1
+echo "before running the code upload command"
 bash <(curl -s https://codecov.io/env) -f .coverage
+echo "after running the code upload command"
